f21787ded590b29a274250f4b81ff2ae137a83e8
Djonathan D'Avila <123762143+DjonathanDAvila@users.noreply.github.com>
2024-04-05 19:38:27 -0300

    Project created

---
+++ xchange/src/main/java/com/cattle/xchange/XchangeApplication.java
@@ -0,0 +1,13 @@
+package com.cattle.xchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XchangeApplication.class, args);
+	}
+
+}
---
+++ xchange/src/test/java/com/cattle/xchange/XchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cattle.xchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
