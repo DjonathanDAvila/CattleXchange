ad77943d4096ec7f74dcde289284ddb9096dad6b
cairo.andrade <cairo.andrade777@gamil.com>
2024-04-17 21:45:07 -0300

    feat(users): Create new user

---
+++ xchange/src/main/java/com/cattle/xchange/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cattle.xchange.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+interface UserRepository extends JpaRepository<User, UUID> {
+}
---
+++ xchange/src/main/java/com/cattle/xchange/domain/user/dtos/UserMinDTO.java
@@ -0,0 +1,29 @@
+package com.cattle.xchange.domain.user.dtos;
+
+import com.cattle.xchange.domain.user.User;
+import jakarta.persistence.Column;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record UserMinDTO(
+        UUID id,
+        String document,
+        String name,
+        String lastName,
+        String email,
+        LocalDate creationDate
+) {
+
+    public UserMinDTO(User user) {
+        this(
+                user.getId(),
+                user.getDocument(),
+                user.getName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getCreationDate()
+        );
+
+    }
+}
