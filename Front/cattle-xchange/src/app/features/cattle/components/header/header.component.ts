0e7860da945cdd471da01763a14b91df720b4cb7
cairo <cairo.andrade777@gmail.com>
2024-06-09 11:03:35 -0300

    feat(header): Home navigation

--- Front/cattle-xchange/src/app/features/cattle/components/header/header.component.ts
+++ Front/cattle-xchange/src/app/features/cattle/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(
+    private router: Router
+  ) {}
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
 }
