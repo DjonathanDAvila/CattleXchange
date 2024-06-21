dc5d308c714ef6b2ff7e8c4d5d77c0b010afe477
Djonathan D'Avila <123762143+DjonathanDAvila@users.noreply.github.com>
2024-06-21 19:41:14 -0300

    :recycle: refector: Refatorado CattleBuyService para receber o token ao invés do id.

--- xchange/src/main/java/com/cattle/xchange/domain/cattleBuy/CattleBuyService.java
+++ xchange/src/main/java/com/cattle/xchange/domain/cattleBuy/CattleBuyService.java
@@ -6,23 +6,17 @@ import com.cattle.xchange.domain.cattleAd.enums.CattleStatusEnum;
 import com.cattle.xchange.domain.cattleBuy.dtos.CattleBuyInsertDTO;
 import com.cattle.xchange.domain.itemBuy.CattleItemBuy;
 import com.cattle.xchange.domain.itemBuy.CattleItemBuyService;
-import com.cattle.xchange.domain.itemBuy.dtos.ItemBuyInsertDTO;
 import com.cattle.xchange.domain.user.User;
 import com.cattle.xchange.domain.user.UserService;
-import com.cattle.xchange.infra.config.security.JwtService;
 import com.cattle.xchange.infra.exception.BadRequestException;
-import io.jsonwebtoken.Jwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -47,35 +41,42 @@ public class CattleBuyService {
     }
 
     public CattleBuy create(CattleBuyInsertDTO cattleBuyInsertDTO) {
-        if (cattleBuyInsertDTO.codUser() == null) {
-            throw new BadRequestException("O código do usuário é obrigatório.");
+
+        User currentUser = userService.getCurrentUser();
+
+        if (currentUser != null) {
+            User buyer = userService.findUserByEmail(currentUser.getEmail());
+
+            if (buyer != null) {
+
+                if (cattleBuyInsertDTO.dataBuy() == null) {
+                    throw new BadRequestException("A data de compra é obrigatória.");
+                }
+
+                User user = userService.findUserById(buyer.getId());
+
+                CattleBuy newCattleBuy = new CattleBuy(
+                        user,
+                        cattleBuyInsertDTO.dataBuy(),
+                        0    // totalValue
+                );
+
+                List<CattleItemBuy> cattleItemBuys = getCattleItemBuyListByUUIDs(cattleBuyInsertDTO.listCodAds()
+                        , newCattleBuy);
+
+                double totalValue = getTotalValueByCattleBuyList(cattleItemBuys);
+
+                newCattleBuy.setTotalValue(totalValue);
+                newCattleBuy.setCattleItemBuyList(cattleItemBuys);
+
+
+                cattleBuyRepository.save(newCattleBuy);
+
+                return newCattleBuy;
+            }
         }
 
-        if (cattleBuyInsertDTO.dataBuy() == null) {
-            throw new BadRequestException("A data de compra é obrigatória.");
-        }
-        User user = userService.findUserById(cattleBuyInsertDTO.codUser());
-
-
-        CattleBuy newCattleBuy = new CattleBuy(
-                user,
-                cattleBuyInsertDTO.dataBuy(),
-                0    // totalValue
-        );
-
-
-        List<CattleItemBuy> cattleItemBuys = getCattleItemBuyListByUUIDs(cattleBuyInsertDTO.listCodAds()
-                                                                        , newCattleBuy);
-
-        double totalValue = getTotalValueByCattleBuyList(cattleItemBuys);
-
-        newCattleBuy.setTotalValue(totalValue);
-        newCattleBuy.setCattleItemBuyList(cattleItemBuys);
-
-
-        cattleBuyRepository.save(newCattleBuy);
-
-        return newCattleBuy;
+        throw new BadRequestException("User not found.");
     }
 
     @Transactional(readOnly = true)
@@ -92,14 +93,14 @@ public class CattleBuyService {
         throw new BadRequestException("User not found.");
     }
 
-    private List<CattleItemBuy> getCattleItemBuyListByUUIDs(List<UUID> cattleAdList, CattleBuy cattleBuy){
+    private List<CattleItemBuy> getCattleItemBuyListByUUIDs(List<UUID> cattleAdList, CattleBuy cattleBuy) {
         List<CattleItemBuy> cattleItemBuysList = new ArrayList<>();
 
 
         for (UUID codAd : cattleAdList) {
             CattleAd cattleAd = cattleAdService.findCattleAdById(codAd).get();
 
-            if (cattleAd != null){
+            if (cattleAd != null) {
                 CattleItemBuy newItemBuy = new CattleItemBuy(
                         cattleBuy,
                         cattleAd,
@@ -114,7 +115,7 @@ public class CattleBuyService {
         return cattleItemBuysList;
     }
 
-    private double getTotalValueByCattleBuyList(List<CattleItemBuy> cattleItemBuys){
+    private double getTotalValueByCattleBuyList(List<CattleItemBuy> cattleItemBuys) {
         double totalValueReturn = 0;
 
         for (var cattleItemBuy : cattleItemBuys) {
